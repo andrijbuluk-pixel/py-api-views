c0a55c4d57eb4f7a0c8a887180cdb1ebfc50945f
Andriy <andrijbuluk@gmail.com>
2026-06-06 10:29:59 +0200

    Aded: endpoint for each entity

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,39 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from cinema.views import (
+    CinemaHallViewSet,
+    MovieViewSet,
+    ActorList,
+    GenreList,
+    ActorDetail,
+    GenreDetail,
+)
 
-from cinema.views import movie_list, movie_detail
+router = DefaultRouter()
+
+router.register(
+    "cinema_halls",
+    CinemaHallViewSet,
+    basename="cinema_halls"
+)
+router.register(
+    "movies",
+    MovieViewSet,
+    basename="movies"
+)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre_list"),
+    path("genres/<int:pk>/",
+         GenreDetail.as_view(),
+         name="genre_detail"
+         ),
+    path("actors/", ActorList.as_view(), name="actor_list"),
+    path("actors/<int:pk>/",
+         ActorDetail.as_view(),
+         name="actor_detail"
+         ),
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
